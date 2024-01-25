1b52eac97cedd4600bb4249b10a1d4ae803c4c91
tituz175 <tobititus01@gmail.com>
2024-01-24 20:39:36 -0700

    task 1 done

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Module Documentation"""
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,3 +18,24 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Get Function Documentation"""
+        value = self._redis.get(key)
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """Get_str Function Documentation"""
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """Get_int Function Documentation"""
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except ValueError:
+            value = 0
+        return value
